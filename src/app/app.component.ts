867e3b267d1ed16e9f331ba40e62e2109ce8400d
Maxo Lotikashvili <88730788+Maxolotikashvili@users.noreply.github.com>
2022-05-11 02:29:49 -0700

    edit-publish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faMailBulk } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+
+  github = faGithub;
+  gmail = faEnvelope;
+  linkedin = faLinkedin;
+
+  outline: string = "outline"
+
+  ngOnInit(): void {
+    
+  }
+
+  changeOutline(mode: boolean) {
+    if (mode === true) {
+      this.outline = "outline-active"
+    } else {this.outline = "outline"}
+  }
 }
